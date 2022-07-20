cb61e13910f9f8185630c8ccc3330248c0ed156a
Gleison <gleison04lemos@gmail.com>
2022-07-20 14:21:02 -0300

    Alterando e manipulando dados

--- passagens/views.py
+++ passagens/views.py
@@ -6,6 +6,13 @@ def index(request):
     contexto = {'form': form}
     return render(request, 'index.html', contexto)
 
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        contexto = {'form': form}
+        return render(request, 'minha_consulta.html', contexto)
+    
+
 
 
 
